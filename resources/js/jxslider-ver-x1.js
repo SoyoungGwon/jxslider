ce19477a9706f85c3dfce0a7d5c605a0e09bbc8c
noranbulb <noranbulb@gmail.com>
2018-07-18 18:19:36 +0900

    first commit

--- resources/js/jxslider-ver-x1.js
+++ resources/js/jxslider-ver-x1.js
@@ -3,29 +3,6 @@
 version( 버전 ) : x1
 date ( 날짜 ) : 2018.07
 
-obj               : 클래스 명
-nView           : 화면에 몇개의 li를 보일것인가 (기본=1)
-nStart          : 앞에 li를 뒤로 붙이는경우. 즉 외부에서 몇번째를 처음부터 보여줄것인가를 정할때. 이걸 정하면 nBackCopy값이 작동 안함  (*이걸 사용하면 nBackCopy값은 0이됨  *loop가 false가 될경우만 작동)
-nBackCopy     : 뒤에 li를 앞으로 붙임.css로 ul의 margin-left로 센터 조정해야함. nStart가 0이여야함 *loop가 false가 될경우만 작동
-nMargin        : li간에 간격(단위 px)
-nUlSpeed       : 이동속도 (기본=400 > 0.4초 )
-bAuto           : 자동 움직임 (true or false:기본)
-nAutoSpeed   : bAuto가 true일경우 몇초 단위로 자동을 시킬것인가 ( 기본=3000 > 3초 )
-bLoop           : 반복으로 로테이션 (true or false:기본)
-bDrag        : pc 또는 mobile에서 드래그 여부 (true or false:기본)
-bResize         : li 객체를 jxbox 박스 기준으로 리사징을 할것인가 여부(true or false:기본)
-nSans           : 드래그 온하고 오프햇을때 드래그 한정도에 따라서 다시 제자리로 오느냐 아님 이동하느냐에 민감도 ( 기본값:10 )
-sDirection      : sDirection, //방향
-bAction         : jxunit 본인을 클릭하면 칸 차이를 인식하여 이동 모바일에선 드래그와 출동하기 때문에 true면 bMobile옵션을 false로 해야함
-bGroupMove  : 움직일때 view가 3일때 한칸씩 움직이는게 기본이지만(단 페이징 있을때 예외) 3칸씩 움직이도록 하고 싶을때
-reCallFnc       : 콜백함수이며 선택된 li의 data-num 을 넘긴다.
-
-
-추가
-
-수정
-
-
 */
 
 
@@ -50,38 +27,42 @@ function JXSLIDER( obj ,   option  )
     this.default =
     {
         nView            : 1 , //화면에 몇개의 객체를 보여줄것인가
-        nStart            : 0 , // 앞객체를 뒤로 붙여서 몇번째 객체를 처음부터 보여줄려할때 , nBackCopy작동안함. *loop가 false가 될경우만 작동 xx
-        nBackCopy     : 0 , // 뒤객체를 앞으로 붙여서 센터 정렬. margin-left로 센터 조정해야함. nStart가 0이여야함 *loop가 false가 될경우만 작동 xx
-        nMargin         : 0 , //sDirection에 따라 margin-right or margin-bottom 값 xx
-        nUlSpeed       : 400 , //움직이는 속도 xx
-        bAuto           : false , //자동롤링 xx
-        nAutoSpeed   : 3000 , //자동롤링 속도 xx
-        bLoop           : true , //무한 반복 xx
+        nStart            : 0 , // 앞객체를 뒤로 붙여서 몇번째 객체를 처음부터 보여줄려할때 , nBackCopy작동안함. *loop가 false가 될경우만 작동
+        nBackCopy     : 0 , // 뒤객체를 앞으로 붙여서 센터 정렬. margin-left로 센터 조정해야함. nStart가 0이여야함 *loop가 false가 될경우만 작동
+        nMargin         : 0 , //sDirection에 따라 margin-right or margin-bottom 값
+        nUlSpeed       : 400 , //움직이는 속도
+        bAuto           : false , //자동롤링
+        nAutoSpeed   : 3000 , //자동롤링 속도
+        bLoop           : true , //무한 반복
         //bMobile        : false , //PC or 모바일 인지를 구분 한다기 보단 드래그 가능여부인데 모바일에서 드래그 하고 싶으면 true PC 에서 드래그할려면 false를 줘야함
-        bResize         : true ,  //li 객체를 jxbox 박스 기준으로 리사징을 할것인가 여부(true:기본 or false) xx
-        nSans           : 10 , //드래그 온하고 오프햇을때 드래그 한정도에 따라서 다시 제자리로 오느냐 아님 이동하느냐에 민감도 ( 기본값:10 ) xx
-        sDirection      : "horizontal" , //움직이는 방향 xx
-        bAction         : false , //li 객체를 클릭하면 칸 차이를 인식하여 이동 링크가 아닌 이동 loop를 true로 바꿔야할듯 xx
+        bResize         : true ,  //li 객체를 jxbox 박스 기준으로 리사징을 할것인가 여부(true:기본 or false)
+        nSans           : 10 , //드래그 온하고 오프햇을때 드래그 한정도에 따라서 다시 제자리로 오느냐 아님 이동하느냐에 민감도 ( 기본값:10 )
+        sDirection      : "horizontal" , //움직이는 방향 or vertical
+        bAction         : false , //li 객체를 클릭하면 칸 차이를 인식하여 이동 링크가 아닌 이동 loop를 true로 바꿔야할듯
         bGroupMove  : false , //nView에 값에 따라서 이동을 한꺼번에 할것인가 아닌가
         bDrag         : true , //드래그 가능
-        // reCallFnc       : function(v)
-        //                     {
-        //                          console.log('default reCallFnc' , v)
-        //                     }
+        oResponsive : [ ] ,
+        reCallFnc       : function(v)
+                            {
+                                 console.log('default reCallFnc' , v)
+                            }
     }
+
+
 
     $.extend(true ,  this.default , option );
 
-    console.log('default  ' , this.default );
+    this.oriView = this.default.nView;
 
 
+
+    //피씨와 모바일 체크
     (function(a,b)
     {
         if( /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)) )
         {
-            //console.log('11 ' , b)
-            //window.location=b
             //모바일
+            //window.location=b
             _this.bMobile = true;
         }
         else
@@ -92,6 +73,8 @@ function JXSLIDER( obj ,   option  )
 
         }
     })(navigator.userAgent||navigator.vendor||window.opera,'m/');
+
+
 
 
     this.jxslider = obj;
@@ -111,17 +94,15 @@ function JXSLIDER( obj ,   option  )
     this.autoTime;
     this.sEase = 'easeOutCubic';
 
-    //console.log('typeof _this.default.reCallFnc   ' , typeof _this.default.reCallFnc , _this.default.reCallFnc == "" , typeof window[ _this.default.reCallFnc ] === 'function')
 
 
-    //this.reCallFnc = ( option["reCallFnc"] != undefined && option["reCallFnc"] != "" ) ? eval( option["reCallFnc"] ) : undefined; //콜벡 함수가 존재하면 담고
 
     //.jxunit의 고유 데이터 넘버를 넣어버린다.
     this.jxwrap.find(">.jx-unit").each(function(i, e)
     {
         $(this).attr("data-num" , i);
         var $num = $("<span class='num'>"+(i+1)+"</span>")
-        $(this).find("a").append($num)
+        $(this).find(".jx-cont").append($num)
     });
 
      //this.bResize = ( option["bResize"]  != undefined && option["bResize"]  != "" ) ? option["bResize"] :  false; //리사이징 여부
@@ -131,11 +112,8 @@ function JXSLIDER( obj ,   option  )
         //세로방향일때고 리사이징일때만
         if ( _this.default.bResize )
         {
-            this.jxslider.css({"height": "100%"});
+            this.jxslider.addClass("jx-vertical");
         }
-         //$(".jxslider .jxbox > .jxwrap > .jxunit img").css ( {"width":"auto" , "height":"inherit" });
-         this.jxwrap.find(">.jx-unit > .jx-cont .thumb img").css ( {"width":"auto" , "height":"inherit" });
-         //this.jxbox.css({"height": "100%"});
     }
 
     //페이징이 존재하면  그룹별 움직임에 true를 넣어버리고 or 그룹별 움직임이 true라면
@@ -150,15 +128,17 @@ function JXSLIDER( obj ,   option  )
         this.bControl = false;
     }
 
+    _this.reSet();
 
-    if ( this.default.bGroupMove )
-    {
-        this.nTotalPaging = Math.ceil( this.nTotal /  _this.default.nView ); //아래 페이징이 있을경우 총 페이징 값
-    }
-    else
-    {
-        this.nTotalPaging = this.nTotal;
-    }
+    // //re
+    // if ( this.default.bGroupMove )
+    // {
+    //     this.nTotalPaging = Math.ceil( this.nTotal /  _this.default.nView ); //아래 페이징이 있을경우 총 페이징 값
+    // }
+    // else
+    // {
+    //     this.nTotalPaging = this.nTotal;
+    // }
 
     if ( _this.default.bLoop )
     {
@@ -252,14 +232,14 @@ function JXSLIDER( obj ,   option  )
 
 
     //여기는 진짜 페이징이 존재할것인가 할때
-    if ( this.bControl )
-    //if ( this.default.bGroupMove )
-    {
+    // if ( this.bControl )
+    // //if ( this.default.bGroupMove )
+    // {
 
-        this.jxcontrol = this.jxslider.find(".jx-control");
-        //console.log('시작 ' , this.bMovIng)
-        this.addEventControl(); //아래 페이징이 있는 경우만 이벤트 추가
-    }
+    //     this.jxcontrol = this.jxslider.find(".jx-control");
+    //     //console.log('시작 ' , this.bMovIng)
+    //     this.addEventControl(); //아래 페이징이 있는 경우만 이벤트 추가
+    // }
 
     if ( _this.default.bLoop )
     {
@@ -311,15 +291,76 @@ function JXSLIDER( obj ,   option  )
     }
 
     if( _this.default.bDrag )
-        {
-            this.addEventDrag();
-        }
-
-
-
-
-
+    {
+        this.addEventDrag();
+    }
 }
+
+
+JXSLIDER.prototype.reSet = function()
+{
+    var _this = this;
+
+
+    //console.log('default  ' , this.default.oResponsive.length , this.default.sDirection , $(window).width() );
+
+    if ( this.default.sDirection == "horizontal")
+    {
+        //가로모드일때만 반응형
+
+        var leng = this.default.oResponsive.length;
+
+        //console.log( leng )
+
+        if ( leng > 0 )
+        {
+
+            this.default.bGroupMove = false;
+
+            for(var i in this.default.oResponsive )
+            {
+                //console.log('aaaa   ' , i  , leng-1 - i )
+
+                if ( $(window).width()+17 >= this.default.oResponsive[ leng-1 - i ].breakpoint )
+                {
+                    this.default.nView = this.oriView;
+
+                }
+                else
+                {
+                    this.default.nView = this.default.oResponsive[ leng-1 - i ].settings.nView;
+                    break;
+                }
+            }//for
+        }//if
+    }//if
+
+    //re
+    if ( this.default.bGroupMove )
+    {
+        this.nTotalPaging = Math.ceil( this.nTotal /  _this.default.nView ); //아래 페이징이 있을경우 총 페이징 값
+    }
+     else
+    {
+        this.nTotalPaging = this.nTotal;
+    }
+
+
+    if ( this.bControl )
+    //if ( this.default.bGroupMove )
+    {
+
+        this.jxcontrol = this.jxslider.find(".jx-control");
+        //console.log('시작 ' , this.bMovIng)
+        this.addEventControl(); //아래 페이징이 있는 경우만 이벤트 추가
+    }
+
+    //console.log('this.nTotalPaging   ' , this.nTotalPaging , this.nTotal , _this.default.nView)
+
+
+
+}//JXSLIDER.prototype.reSet
+
 
 //본인 자신을 클릭하면 움직임 추가
 JXSLIDER.prototype.addEventAction = function(b)
@@ -896,6 +937,9 @@ JXSLIDER.prototype.addEventControl = function()
     //.jxunit 갯수만큼 버튼을 생성하고
 
     //console.log('addEventControl    ' , _this.default.nView)
+    //re
+
+    _this.jxcontrol.empty();
 
     for(var i = 0 ; i < _this.nTotalPaging ; i++ )
     {
@@ -907,11 +951,31 @@ JXSLIDER.prototype.addEventControl = function()
 
     _this.jxcontrol.find(".jx-cbtn").eq(_this.nCurrPaging).addClass("on").trigger("click");
 
+    _this.jxcontrol.find(".jx-cbtn").unbind("click");
     _this.jxcontrol.find(".jx-cbtn").click(function(e)
     {
 
-        var nChr = _this.nCurrPaging - $(this).index();
-        _this.nCurrPaging = $(this).index();
+        _this.select( $(this).index() );
+
+        if ( _this.default.bAuto ) { _this.addEventAuto(true); }
+
+    }).mouseenter(function(event)
+    {
+        if ( _this.default.bAuto ) { _this.addEventAuto(false); }
+    }).mouseleave(function(event)
+    {
+        if ( _this.default.bAuto ) { _this.addEventAuto(true); }
+        _this.bMovIng = false;
+    });
+
+
+}//JXSLIDER.prototype.addEventControl
+
+JXSLIDER.prototype.select = function(v)
+{
+     var _this = this;
+     var nChr = _this.nCurrPaging - v;
+        _this.nCurrPaging = v;
         //_this.nCurr = _this.default.nView * _this.nCurrPaging; //몇번째 .jxunit 인지를 담는다
         _this.nCurr = _this.nCurrPaging; //몇번째 .jxunit 인지를 담는다
 
@@ -932,7 +996,7 @@ JXSLIDER.prototype.addEventControl = function()
             }
             else
             {
-                _this.rmv( $(this).index() *  view );
+                _this.rmv( v *  view );
             }
         }
 
@@ -945,24 +1009,11 @@ JXSLIDER.prototype.addEventControl = function()
             }
             else
             {
-                _this.lmv( $(this).index() *  view );
+                _this.lmv( v *  view );
             }
         }
 
-
-        if ( _this.default.bAuto ) { _this.addEventAuto(true); }
-
-    }).mouseenter(function(event)
-    {
-        if ( _this.default.bAuto ) { _this.addEventAuto(false); }
-    }).mouseleave(function(event)
-    {
-        if ( _this.default.bAuto ) { _this.addEventAuto(true); }
-        _this.bMovIng = false;
-    });
-
-
-}//JXSLIDER.prototype.addEventControl
+}//JXSLIDER.prototype.select
 
 
 //드래그 이벤트
@@ -983,7 +1034,7 @@ JXSLIDER.prototype.addEventDrag = function()
     }
     else
     {
-        sEventStart  = "mousedown"; sEventMove = "mousemove"; sEventEnd = "click mouseup mouseleave";
+        sEventStart  = "mousedown"; sEventMove = "mousemove"; sEventEnd = "click  mouseleave";
     }
 
     var nSX = 0 , nSY = 0 ; // 클릭시에 처음 지점
@@ -1227,8 +1278,6 @@ JXSLIDER.prototype.addEventDrag = function()
         else
         {
             //loop 가 아닐때/
-            //console.log('  loop 가 아닐때    _this.nEX  '  , _this.nEX)
-
             if ( _this.default.sDirection == "horizontal")
             {
                 _this.jxwrap.css( { "left" : _this.nEX + nCX } ); //ul을 이동하고
@@ -1244,14 +1293,17 @@ JXSLIDER.prototype.addEventDrag = function()
     {
         // e.stopPropagation();
 
+
         nMX = nPX = undefined ; //드래그 왔다 갔다 담는 값을 초기화
 
         _this.bPcIng = false; //피시에서 드래그 계속 되는것을 방지
 
         if ( _this.default.bAuto  ) { _this.addEventAuto(true); }
 
+
         if( _this.default.bLoop )
         {
+
             if ( _this.default.sDirection == "horizontal")
             {
                 //루프 좌우
@@ -1270,6 +1322,7 @@ JXSLIDER.prototype.addEventDrag = function()
                 }
                 else
                 {
+
                     if ( Math.abs(nCX) < Math.abs(nCY)  )
                     {
                         return;
@@ -1306,7 +1359,7 @@ JXSLIDER.prototype.addEventDrag = function()
                 }
                 else
                 {
-                    console.log("상하 루프 아주 작게움직였다")
+
                     if ( Math.abs(nCY) < Math.abs(nCX)  )
                     {
                         //console.log("아래로 스크롤 내릴때는 좌우 움직이지 마")
@@ -1384,8 +1437,7 @@ JXSLIDER.prototype.addEventDrag = function()
             {
 
                 //var npa = (_this.nAddStep == 1)? 0 : 1;
-                var npa
-
+                var npa;
                 if ( !_this.default.bGroupMove )
                 {
                     npa = (  Math.abs( _this.nAddStep )  == 1)? 0 : Math.abs( _this.nAddStep ) -1;
@@ -1395,12 +1447,9 @@ JXSLIDER.prototype.addEventDrag = function()
                     npa = 0;
                 }
 
-                //console.log('left end  _this.nAddStep ' , _this.nAddStep , npa)
 
-                //console.log( "left end" , npa , _this.nAddStep - (_this.nAddStep-1) )
                 for(var i = 0 ; i < Math.abs( _this.nAddStep ) - npa   ; i++)
                 {
-                    //console.log('sss  ' , i )
                     var $jxunit = _this.jxwrap.find(">.jx-unit:first");
                     _this.jxwrap.append( $jxunit );
                 }
@@ -1444,12 +1493,9 @@ JXSLIDER.prototype.addEventDrag = function()
 
            var nAurr;
 
-           //console.log('end  aa ' , nAurr  , _this.nAddStep)
-
             //if ( _this.bControl  || _this.default.bGroupMove )
             if ( _this.default.bGroupMove )
             {
-
                 //컨트롤이 있을땐  _this.default.nView 단위로 이동하면 되고
                 //nAurr = ( _this.default.nView == 1 )? 1 : _this.default.nView ; //몇칸을 더할것인지
                 nAurr = _this.default.nView ; //몇칸을 더할것인지
@@ -1466,8 +1512,6 @@ JXSLIDER.prototype.addEventDrag = function()
                 {
                     nAurr = Math.ceil( Math.abs ( nCY / _this.nMove  ) ); //몇칸을 더할것인지
                 }
-
-                console.log('end  bb ' , nAurr )
 
             }
 
@@ -1510,21 +1554,11 @@ JXSLIDER.prototype.addEventDrag = function()
                     //nAurr++; //무조건 반내림을 해서:ceil 왼쪽으로 할땐 1을 더해주자
                     _this.nCurr += nAurr;
 
-                    if ( _this.bControl )
+                    if ( _this.nCurr  > _this.nTotal  - 1 )
                     {
-                        if ( _this.nCurr  > _this.nTotal  - 1 )
-                        {
-                            _this.nCurr = _this.nTotal  - 1 ;
-                        }
+                        _this.nCurr = _this.nTotal  - 1 ;
                     }
-                    else
-                    {
-                        if ( _this.nCurr  > _this.nTotal   - _this.default.nView )
-                        {
-                            _this.nCurr = _this.nTotal  - _this.default.nView ;
-                        }
 
-                    }
                 }
 
             }//notLoopLeftUpDrag
@@ -1563,6 +1597,9 @@ JXSLIDER.prototype.addEventDrag = function()
             }//notLoopRightDownDrag
 
             //console.log(  " 드래그" ,  _this.nCurr  ,   Math.round( _this.nCurr / _this.default.nView ) , Math.ceil( _this.nCurr / _this.default.nView )  , Math.floor( _this.nCurr / _this.default.nView ) );
+
+            console.log('ntx  ' , _this.nMove , _this.nCurr)
+
 
             var ntx = _this.nMove * _this.nCurr  * -1;
             // var ntx = _this.nMove * (  Math.floor( _this.nCurr / _this.default.nView )  * _this.default.nView  )  * -1;
@@ -1693,10 +1730,15 @@ JXSLIDER.prototype.resize = function()
 
     //console.log('re ' , this.default.nView)
     //this.nMove =  this.jxbox.innerWidth() / this.nView ; // 한번씩 이동할 거리를 저장해둠
+
+    this.reSet();
+
     if ( _this.default.sDirection == "horizontal")
     {
         this.nMove =  Math.ceil(this.jxbox.innerWidth() / this.default.nView) ; // 한번씩 이동할 거리를 저장해둠
         this.jxwrap.find(">.jx-unit").css({"width": Math.ceil( this.jxbox.innerWidth() / this.default.nView )-_this.default.nMargin , "margin-right": _this.default.nMargin }); // .jx-unit의 가로사이즈와 간격 ( 추후에 가로냐 세로냐를 추가해야함)
+
+        //console.log('wid ' , this.jxbox.width() , this.jxbox.innerWidth() ,   this.jxbox.outerWidth(true)   , ( this.jxbox.innerWidth() / this.default.nView )-_this.default.nMargin    )
         this.jxwrap.css({"width" : this.nMove * this.nTotal }) // ul의 가로 사이즈를 .jx-unit의 갯수만큼
     }
     else
